4e3a22c0537a048156c9bcc044d83f53a565ffb8
mathiasiriarte <mathias.iriarte@hotmail.com>
2021-06-05 11:53:55 -0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './style.css';
 
+const Nombre = 'Mathias';
+
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hola {Nombre}!</h1>
     </div>
   );
 }
